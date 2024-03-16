9db30fab80739b8d21e5c13cb48c764bb6a34b03
Claquettes <math@claq.fr>
2024-03-16 17:57:47 +0100

    added keyListener to the board on the controller

--- src/controllers/GameController.java
+++ src/controllers/GameController.java
@@ -1,21 +1,30 @@
 package controllers;
 
+import javax.swing.JPanel;
 import javax.swing.Timer;
-
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import models.Grid;
 
 public class GameController {
-
     private Timer timer;
     private Grid grid;
-    
-    public GameController(Grid Grid) {
-        System.out.println("GameController");
 
+    public GameController(Grid Grid, JPanel board) {
+        System.out.println("GameController");
         this.grid = Grid;
         timer = new Timer(500, grid);
-
         timer.start();
+
+        board.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyChar() == 'd') {
+                    System.out.println("Debug: 'd' key pressed");
+                }
+            }
+        });
+        board.setFocusable(true);
     }
 
     public void start() {

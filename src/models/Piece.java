69353905799732094c9fc318fe33777c3691186a
TaTu <arthur.rigonnet@gmail.com>
2024-03-21 12:09:27 +0100

    Refactor Piece class and add PieceShape enum

--- src/models/Piece.java
+++ src/models/Piece.java
@@ -2,70 +2,30 @@ package models;
 
 import java.util.Random;
 
-public enum Piece {
-    I(new int[][]{
-        {0, 0, 0, 0},
-        {1, 1, 1, 1},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}
-    }, 1, 5, 0),
-    O(new int[][]{
-        {0, 0, 0, 0},
-        {0, 1, 1, 0},
-        {0, 1, 1, 0},
-        {0, 0, 0, 0}
-    }, 2, 5, 0),
-    T(new int[][]{
-        {0, 0, 0, 0},
-        {0, 0, 1, 0},
-        {0, 1, 1, 1},
-        {0, 0, 0, 0}
-    }, 3, 5, 0),
-    S(new int[][]{
-        {0, 1, 0, 0},
-        {0, 1, 1, 0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 0}
-    }, 4, 5, 0),
-    Z(new int[][]{
-        {0, 0, 1, 0},
-        {0, 1, 1, 0},
-        {0, 1, 0, 0},
-        {0, 0, 0, 0}
-    }, 5, 5, 0),
-    J(new int[][]{
-        {0, 0, 1, 0},
-        {0, 0, 1, 0},
-        {0, 1, 1, 0},
-        {0, 0, 0, 0}
-    }, 6, 5, 0),
-    L(new int[][]{
-        {0, 1, 0, 0},
-        {0, 1, 0, 0},
-        {0, 1, 1, 0},
-        {0, 0, 0, 0}
-    }, 7, 5, 0);
+public class Piece {
 
-    private int[][] shape;
+    private PieceShape shape;
+    private int color;
     private int xpos;
     private int ypos;
 
-    private int color;
-
-    Piece(int[][] shape, int color, int xpos, int ypos) {
-
+    public Piece(PieceShape shape, int xpos, int ypos) {
         this.shape = shape;
-        this.color = color;
+        this.color = 0;
         this.xpos = xpos;
         this.ypos = ypos;
     }
 
     public int[][] getShape() {
-        return this.shape;
+        return this.shape.getShape();
     }
 
     public int getColor() {
         return this.color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
     }
     
     public int[] getPos(){
@@ -79,9 +39,13 @@ public enum Piece {
     }
 
     public static Piece placeRandomPiece(int[][] PieceGrid) {
-        // Choose a random piece
-        Piece[] pieces = Piece.values();
-        Piece randomPiece = pieces[new Random().nextInt(pieces.length)];
+        PieceShape[] shapes = PieceShape.values();
+        PieceShape randomShape = shapes[new Random().nextInt(shapes.length)];
+
+
+        // Create a new piece with the random shape
+        Piece randomPiece = new Piece(randomShape, 5, 0);
+        randomPiece.setColor(randomShape.getColor());
     
         // Place the piece at the middle of the grid
         for (int i = 0; i < 4; i++) {

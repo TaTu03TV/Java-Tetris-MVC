e01881000f9a9e4518f044240ceb545e3871aded
Claquettes <mathieu.ponton@etu.univ-lyon1.fr>
2024-04-09 12:11:32 +0200

    detects when controlling piece while in caps lock

--- src/controllers/GameController.java
+++ src/controllers/GameController.java
@@ -15,10 +15,10 @@ public class GameController {
         board.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                if (e.getKeyChar() == 'q') {
+                if (e.getKeyChar() == 'q' || e.getKeyChar() == 'Q') {
                     grid.movePieceLeft();
                 }
-                if (e.getKeyChar() == 'd') {
+                if (e.getKeyChar() == 'd' || e.getKeyChar() == 'D') {
                     grid.movePieceRight();
                 }
                 if (e.getKeyCode() == KeyEvent.VK_SPACE) {
@@ -26,16 +26,16 @@ public class GameController {
                         grid.descendPiece();
                     }
                 }
-                if(e.getKeyChar() == 's'){
+                if(e.getKeyChar() == 's' || e.getKeyChar() == 'S'){
                     if(grid.canDescend()){
                         grid.descendPiece();
                     }
                 }
-                if(e.getKeyChar() == 'z'){
+                if(e.getKeyChar() == 'z' || e.getKeyChar() == 'Z'){
                     if(grid.canRotate()){
                         grid.rotatePiece();
                     }
-                }if(e.getKeyChar() == 'p'){
+                }if(e.getKeyChar() == 'p' || e.getKeyChar() == 'P'){
                     grid.pause();
                 }
             } 

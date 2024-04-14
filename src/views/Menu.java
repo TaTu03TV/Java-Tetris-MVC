1d3327cf96e3a13a340d6566863acc371379b1d4
Claquettes <mathieu.ponton@etu.univ-lyon1.fr>
2024-04-14 11:22:28 +0200

    better menu

--- src/views/Menu.java
+++ src/views/Menu.java
@@ -4,28 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
 /**
  * Class to create the main menu of the game
  * 
- * This class is used to create the main menu of the game. It contains the play, settings, and quit buttons. It also contains methods to start the game, open the settings, and quit the game.
+ * This class is used to create the main menu of the game. It contains the play,
+ * settings, and quit buttons. It also contains methods to start the game, open
+ * the settings, and quit the game.
  * 
  * @version 1.0
  * @since 2024-04-14
  */
-public class Menu extends JFrame {
 
-    /**
-     * Constructor for the Menu class
-     * 
-     * @see Main
-     */
+public class Menu extends JFrame {
     public Menu() {
-        setSize(300, 300);
+        setSize(200, 200); // Make the menu smaller
         setLayout(new GridLayout(3, 1));
-        JButton playButton = new JButton("Play");
-        JButton settingsButton = new JButton("Settings");
-        JButton quitButton = new JButton("Quit");
+
+        // Create buttons with custom styles
+        JButton playButton = createStyledButton("Play");
+        JButton settingsButton = createStyledButton("Settings");
+        JButton quitButton = createStyledButton("Quit");
 
         playButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -49,5 +47,13 @@ public class Menu extends JFrame {
         add(playButton);
         add(settingsButton);
         add(quitButton);
+    }
+
+    private JButton createStyledButton(String text) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Arial", Font.BOLD, 14)); // Set font
+        button.setBackground(Color.LIGHT_GRAY); // Set background color
+        button.setForeground(Color.BLACK); // Set text color
+        return button;
     }
 }
